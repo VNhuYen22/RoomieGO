098f6aed8dd7f05571e03c916e97aa72a057a5b5
VoDucHuy <vdhuy2801@gmail.com>
2025-03-19 21:01:13 +0700

    update

--- roomiego_fe/src/components/SearchBar.js
+++ roomiego_fe/src/components/SearchBar.js
@@ -1,35 +1,39 @@
 import React, { useState } from "react";
 import "../styles/SearchBar.css";
-import Coin from "../assets/coin.png";
 import User from "../assets/user.png";
-import Location from "../assets/location.png";
+import Find from "../assets/find.png";
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = () => {
     alert(`Tìm kiếm: ${searchQuery}`);
   };
+
   return (
-    <div>
-      <div className="search-bar">
-        <div className="search-top">
-            <h3>Find Room</h3>
-            <ul>
-                <li><img src={Coin} alt="" className="coin-icon"/>Price</li>
-                <li><img src={User} alt="" className="user-icon" />Person</li>
-                <li><img src={Location} alt="" className="location-icon" />Select Location</li>
-                <button onClick={handleSearch}>Search</button>
-            </ul>
-        </div>
-        <div className="search-bottom">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search..."
-          />
-          
-        </div>
+    <div className="search-bar">
+        <img src={Find} alt="Find Icon" className="find-icon" />
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Find your roomie ?"
+      />
+      <div className="search-top">
+        <ul>
+          <li>
+            <img src='' alt="" className="location-icon" />
+            Check In
+          </li>
+          <li>
+            <img src='' alt="" className="location-icon" />
+            Check Out
+          </li>
+          <li>
+            <img src="" alt="" className="user-icon" />
+            Guests
+          </li>
+        </ul>
+        <button onClick={handleSearch}>Let's go!</button>
       </div>
     </div>
   );
