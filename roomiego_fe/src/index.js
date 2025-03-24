b2f979c2abff77bac9ee0f36d36a39e0c95b351d
d.ngochien <133486635+dinhngoc203@users.noreply.github.com>
2025-03-25 00:09:18 +0700

    Update index.js

--- roomiego_fe/src/index.js
+++ roomiego_fe/src/index.js
@@ -9,7 +9,6 @@ import Login from './pages/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  {/* <App /> */}
-  <Login/>
+   <App />
 </React.StrictMode>
 );
