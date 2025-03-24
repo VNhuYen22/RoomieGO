5a69d694d4b9e7331b34298a334152e19116b596
akangochien Ngochien <akangochien@gmail.com>
2025-03-24 22:57:54 +0700

    aaaa

--- roomiego_fe/src/App.js
+++ roomiego_fe/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/change-password" element={<ChangePassword />} /> */}
+        <Route path="./pages/Login.js" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
