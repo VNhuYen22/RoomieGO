b3b91934c443879d1b5d65fbb62da99ce91e0e91
NhuYenCMU <vothinhuyen22042003@gmail.com>
2025-03-25 11:52:31 +0700

    change file name

--- roomiego_fe/src/App.js
+++ roomiego_fe/src/App.js
@@ -1,6 +1,42 @@
-<<<<<<< HEAD
+// import "./App.css";
+// import Navbar from "./components/NavBar";
+// import Home from "./pages/Home";
+// import SearchBar from "./components/SearchBar";
+// import Footer from './components/Footer';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Room from "./pages/Room";
+// import About from "./pages/About";
+// import ResultRoom from "./pages/Result_Room";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Navbar />
+//         <SearchBar />
+        
+//         <Routes>
+//           <Route path="home" element={<Home/>} />
+//           <Route path="about" element={<About/>} />
+//           <Route path="room" element={<Room/>} />
+//           <Route path="ResultRoom" element={<ResultRoom/>} />
+//           <Route path="./pages/Login.js" element={<Login />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+import Navbar from "./components/Navbar";
+
+
+import SettingsPage from "./pages/SettingsPage";
+import Chatbox from "./pages/Chatbox";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 import "./App.css";
-import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import SearchBar from "./components/SearchBar";
 import Footer from './components/Footer';
@@ -8,38 +44,50 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Room from "./pages/Room";
 import About from "./pages/About";
 import ResultRoom from "./pages/Result_Room";
-function App() {
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login.js";
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
-    <div className="App">
+    <div data-theme={theme}>
       <Router>
-        <Navbar />
-        <SearchBar />
-        
-        <Routes>
-          <Route path="home" element={<Home/>} />
-          <Route path="about" element={<About/>} />
-          <Route path="room" element={<Room/>} />
-          <Route path="ResultRoom" element={<ResultRoom/>} />
-        </Routes>
-        <Footer />
+          <Navbar />
+          <SearchBar />
+
+      <Routes>
+        <Route path="home" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="room" element={<Room/>} />
+        <Route path="ResultRoom" element={<ResultRoom/>} />
+        <Route path="login" element={<Login />} />
+        <Route path="/chat" element={<Chatbox />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+
+      <Toaster />
+          <Footer />
       </Router>
     </div>
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/change-password" element={<ChangePassword />} /> */}
-        <Route path="./pages/Login.js" element={<Login />} />
-      </Routes>
-    </Router>
->>>>>>> 15b3c45329f754a3ff919b0b44ec1b3c0c43d70e
   );
-}
-
+};
 export default App;
+
