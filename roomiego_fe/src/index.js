969de9f1a8e343f4b2a50fd2bf777dcfd0930456
d.ngochien <133486635+dinhngoc203@users.noreply.github.com>
2025-04-01 01:43:55 +0700

    Update index.js

--- roomiego_fe/src/index.js
+++ roomiego_fe/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import FilterModal from './pages/FilterModal';
+
 
 
 
@@ -13,10 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 <<<<<<< HEAD
-  {/* <App /> */}
-  <FilterModal/>
- 
-=======
+  
    <App />
 >>>>>>> b1d8ab37fef4a181425d6b90bb48d5c929aa86c7
 </React.StrictMode>
